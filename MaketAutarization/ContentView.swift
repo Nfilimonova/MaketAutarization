1542b1bed5735647461ec30469e916a664a2a8e7
Anastasia <Nfilimonova97@yandex.ru>
2021-06-05 09:26:55 +0300

    first commit

--- MaketAutarization/ContentView.swift
+++ MaketAutarization/ContentView.swift
@@ -8,11 +8,71 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var email = ""
+    @State var pass = ""
+    
+    
+    
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack{
+            
+            Image("Glogo")
+                .resizable()
+                .aspectRatio(contentMode: /*@START_MENU_TOKEN@*/.fill/*@END_MENU_TOKEN@*/)
+                .frame(width: 183, height: 183, alignment: .center)
+            
+               
+        Text("EMAIL")
+            .frame(width: 350, height: nil, alignment: .leading)
+            .font(.custom("Arial-ExtraBold", size: 13))
+            .padding(.top,10)
+          
+                HStack{
+                Image (systemName: "envelope")  .foregroundColor(.gray)
+                    .padding()
+                TextField("name@email.com", text: $email).padding(.horizontal)
+                }
+                .overlay(RoundedRectangle(cornerRadius: 8) .stroke(Color(UIColor.gray).opacity(1),lineWidth: 1))
+                .padding(.horizontal, 20)
+                
+       
+        Text("PASSWORD").font(.custom("Arial-ExtraBold", size: 13))
+            .frame(width: 350, height: nil, alignment: .leading)
+            .padding(.top,10)
+                HStack{
+                    Image(systemName: "lock")  .foregroundColor(.gray)
+                        .padding()
+            TextField("**********", text: $pass).padding(.horizontal)
+                    
+            }
+                .overlay(RoundedRectangle(cornerRadius: 8) .stroke(Color(UIColor.gray).opacity(1),lineWidth: 1))
+                .padding(.horizontal,20)
+            
+            Button(action: {
+               
+            }, label: {
+                Text("SIGN UP")
+                    .font(.custom("Arial", size: 12))
+                    .foregroundColor(.white)
+                    .frame(width: 290, height: 40, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                    .background(Color(.blue))
+                    .border(Color(.blue), width: /*@START_MENU_TOKEN@*/1/*@END_MENU_TOKEN@*/)
+                    .cornerRadius(20)
+                    .padding()
+            })
+            HStack{
+            Text("Already have an accaunt?")
+                .font(.custom("Arial", size: 13))
+                .foregroundColor(.gray)
+            Text("Login")
+                .font(.custom("Arial", size: 13))
+                .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+            }
+        }
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
